9a8f8b25b3d86028a84db15f5a119800fa59b21f
Veronica Perez <veroperezz3912@gmail.com>
2023-08-30 11:42:36 -0300

    Corregi error de armado de blog

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -17,6 +17,7 @@ def index():
         ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
+
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
@@ -41,7 +42,21 @@ def create():
             return redirect(url_for('blog.index'))
 
     return render_template('blog/create.html')
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
 
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+
+    return post
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
 def update(id):
