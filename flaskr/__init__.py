2c06e5f2a8669b38977cb88b5bd2423d87ed7ea9
Veronica Perez <veroperezz3912@gmail.com>
2023-08-30 11:39:08 -0300

    Creacion pagina web, paso de 1a 5

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -31,5 +31,27 @@ def create_app(test_config=None):
     
     from . import db
     db.init_app(app)
+
+
+
     
+    return app
+
+def create_app():
+    app = ...
+    # existing code omitted
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    return app
+def create_app():
+    app = ...
+    # existing code omitted
+
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
