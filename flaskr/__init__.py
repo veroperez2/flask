5cf3200d2f926194c50fa76e3adcb335e561fbb7
veroperezz391 <veroperezz391@gmail.com>
2023-09-06 09:23:23 -0300

    aregle los templates de post

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -32,23 +32,8 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
-
-
-    
-    return app
-
-def create_app():
-    app = ...
-    # existing code omitted
-
-    from . import blog
-    app.register_blueprint(blog.bp)
-    app.add_url_rule('/', endpoint='index')
-
-    return app
-def create_app():
-    app = ...
-    # existing code omitted
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     from . import blog
     app.register_blueprint(blog.bp)
